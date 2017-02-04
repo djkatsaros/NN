ae43be1af867500a0395e91cb8e1a3f35889a10a
Oliver Gilbert Spiro <spiro@rcf1537-2.math.umass.edu>
2017-02-03 22:45:33 -0500

    Data holder now just a softlink generator script, CNN runs but is not OK yet

--- MRIDataHolder.py
+++ MRIDataHolder.py
@@ -2,68 +2,41 @@
 import matplotlib
 import numpy as np
 import os
+import shutil
 import csv
-import fnmatch
 from skimage import  img_as_float
 from skimage.io import imread
 import string
-import matplotlib.pyplot as plt
-class MRIDataHolder(object):
-    def __init__(self, path, img_size=(256,256,3),use_csv=True):
-        self.data_paths = []
-        self.diag_birad = []
-        self.diag_ben_mal = []
-        if use_csv:
-            path_idx = 12
-            birad_idx = 8
-            ben_mal_idx = 9
-            with open("mass_case_description_train_set.csv",'r') as csvfile:
-                phenotypes = csv.reader(csvfile)
-                for row in phenotypes:
-                    self.data_paths.append("/home/spiro/ROI_AND_CROPPED/DOI/" +
-string.replace(row[path_idx],'\\','/')  + ".png")
+def gen_class_links(link_path,use_csv = True, birad = False):
+    if birad == True:
+        raise NameError("Sorry, birad sorting not yet implemented")
+    subjects = {}
+    if use_csv:
+        path_idx = 12
+        birad_idx = 8
+        ben_mal_idx = 9
+        with open("mass_case_description_train_set.csv",'r') as csvfile:
+            phenotypes = csv.reader(csvfile)
+            for row in phenotypes:
+                next(phenotypes)
+                diagnosis = row[birad_idx] if birad == True else row[ben_mal_idx]
+                #fix slash weirdness
+                path = string.replace(row[path_idx],'\\','/')
+                png_path = "/home/spiro/ROI_AND_CROPPED/DOI/" + path + ".png"
+                subjects.update({ png_path : str(diagnosis)})
                     #TODO:make path general
-                    self.diag_birad.append(row[birad_idx])
-                    self.diag_ben_mal.append((row[ben_mal_idx]))
-            self.data_paths = self.data_paths[1:]
-            print self.data_paths[0], self.data_paths[1]
-            self.diag_birad = self.diag_birad[1:]
-            self.diag_ben_mal = self.diag_ben_mal[1:] #chop off column names 
-            #remove masks
-            for idx,p in enumerate(self.data_paths):
-                im = imread(p)
-                if not np.any((im > 0) & (im < 1)):
-                    del self.data_paths[idx]
-                    del self.diag_birad[idx]
-                    del self.diag_ben_mal[idx]
+        #os.makedirs('./Benign','./Malignant')
+        for p,d in subjects.iteritems():
+            im = img_as_float(imread(p)) #float --> signif slowdown?
+            if np.any((im > 0) & (im < 1)):
+                filename = string.replace(p, '/', '_')
+                if d.find("BENIGN"):
+                    os.symlink(p,link_path + '/Benign/' + filename)
+                else:
+                    os.symlink(p, link_path + '/Malignant/' + filename)
 
-        #TODO:fix later for use when csv not available
-        #for root, dir, file in os.walk("ROI_AND_CROPPED"):
-         #   for f in [os.path.join(root,x) for x in file if fnmatch.fnmatch(x,'*.png')]:
-          #      self.data_paths.append(f)
-    def batchGenerator(self, batch_size):
-        i = 0
-        while batch_size*i < len(self.data_paths):
-            images = []
-            batch_end = batch_size*(i+1) if batch_size*(i+1) < len(self.data_paths) else len(self.data_paths)-1
-            print batch_end
-            for p in self.data_paths[i*batch_size:batch_end]:
-                im = img_as_float(imread(p))
-                if np.any(im > 1): #check if it is an ROI and not a mask
-                    images.append(im)
-            if not images:
-                i+=1
-                continue
-            yield images
-            i+=1
 
 
 
 if __name__ == "__main__":
-	test = MRIDataHolder("ROI_AND_CROPPED")
-	batchGen = test.batchGenerator(1)
-	print test.data_paths
-	batch = batchGen.next()
-        print batch[0].shape
-	#plt.imshow(batch[3])
-	#plt.show()
+    gen_class_links(link_path = "/home/spiro/link_path")
